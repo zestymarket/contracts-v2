7ed8fe65958de5d5736a8e05b40d77fe84305981
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-05-16 22:24:17 +0800

    include matic deployment settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -34,7 +34,11 @@ module.exports = {
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
+    },
+    matic: {
+      url: `https://rpc-mainnet.maticvigil.com`,
+      accounts: [`0x${process.env.PRIVATE_KEY_MATIC}`]
+    },
   }
 };
 
