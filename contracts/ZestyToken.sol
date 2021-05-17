2e4af6444985216a394da63b755d347d47c708bd
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-05-17 21:01:02 +0800

    add ERC20Burnable

--- contracts/ZestyToken.sol
+++ contracts/ZestyToken.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.7.0;
 // Import OpenZeppelin Contract
 import "./openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
-import "./openzeppelin/contracts/access/Ownable.sol";
+import "./openzeppelin/contracts/token/ERC20/ERC20Burnable.sol"
 
 // This ERC-20 contract mints the specified amount of tokens to the contract creator.
-contract ZestyToken is ERC20Capped, Ownable {
-    uint256 public constant maxCap = (10 ** 8) * (10 ** 18);
-    // uint256 public constant initialMint = (10 ** 6) * (10 ** 18);
+contract ZestyToken is ERC20Capped, ERC20Burnable {
+    uint256 public constant maxCap = (4206913378008) * (10 ** 18);
 
     constructor() 
         ERC20("Zesty Market Token", "ZESTY") 
