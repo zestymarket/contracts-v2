8ee05fdf5c728f45577a10c500b24770036a8ff3
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-05-27 00:37:27 +0800

    add time based test

--- test/ZestyMarket_ERC20_V1.js
+++ test/ZestyMarket_ERC20_V1.js
@@ -1,25 +1,32 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
+const { time } = require('@openzeppelin/test-helpers');
 
 describe('ZestyMarket_ERC20_V1', function() {
-  let streamGame;
   let signers;
+  let zestyMarket;
+  let timeNow;
 
   beforeEach(async () => {
     signers = await ethers.getSigners();
+    timeNow = await time.latest();
+    timeNow 
+    timeNow = timeNow.toNumber();
 
-    const ZestyToken = await ethers.getContractFactory('ZestyToken');
+    let ZestyToken = await ethers.getContractFactory('ZestyToken');
     zestyToken = await ZestyToken.deploy();
     await zestyToken.deployed();
 
-    const ZestyNFT = await ethers.getContractFactory('ZestyNFT');
+    let ZestyNFT = await ethers.getContractFactory('ZestyNFT');
     zestyNFT = await ZestyNFT.deploy(zestyToken.address);
-    await zestyNFT.deployed();
 
-    const ZestyMarket = await ethers.getContractFactory('ZestyMarket_ERC20_V1');
+    let ZestyMarket = await ethers.getContractFactory('ZestyMarket_ERC20_V1');
     zestyMarket = await ZestyMarket.deploy(zestyToken.address, zestyNFT.address);
     await zestyMarket.deployed();
 
+    await zestyNFT.deployed();
+    await zestyNFT.mint('testUri');
+    await zestyNFT.approve(zestyMarket.address, 0);
     await zestyToken.approve(zestyMarket.address, 100000000);
     await zestyToken.transfer(signers[1].address, 100000);
     await zestyToken.connect(signers[1]).approve(zestyMarket.address, 100000000);
@@ -34,4 +41,42 @@ describe('ZestyMarket_ERC20_V1', function() {
     expect(data.buyer).to.equal(signers[0].address);
     expect(data.uri).to.equal('testUri');
   });
+
+  it('It should only allow a seller to deposit and withdraw an NFT', async function() {
+    await zestyMarket.sellerNFTDeposit(0, 1);
+    let data = await zestyMarket.getSellerNFTSetting(0);
+    expect(data.tokenId).to.equal(0);
+    expect(data.seller).to.equal(signers[0].address);
+    expect(data.autoApprove).to.equal(1);
+    expect(data.inProgressCount).to.equal(0);
+
+    await expect(zestyMarket.connect(signers[1]).sellerNFTWithdraw(0)).to.be.reverted;
+
+    await zestyMarket.sellerNFTWithdraw(0);
+    data = await zestyMarket.getSellerNFTSetting(0);
+    expect(data.tokenId).to.equal(0);
+    expect(data.seller).to.equal(ethers.constants.AddressZero);
+    expect(data.autoApprove).to.equal(0);
+    expect(data.inProgressCount).to.equal(0);
+  });
+
+  it('It should prevent a seller from withdrawing an NFT once an auction takes place', async function() {
+    await zestyMarket.sellerNFTDeposit(0, 1);
+    let data = await zestyMarket.getSellerNFTSetting(0);
+    expect(data.tokenId).to.equal(0);
+    expect(data.seller).to.equal(signers[0].address);
+    expect(data.autoApprove).to.equal(1);
+    expect(data.inProgressCount).to.equal(0);
+
+    await zestyMarket.sellerAuctionCreate(
+      0,
+      timeNow + 100,
+      timeNow + 10000,
+      timeNow + 101,
+      timeNow + 10001,
+      100
+    );
+
+    await expect(zestyMarket.connect(signers[1]).sellerNFTWithdraw(0)).to.be.reverted;
+  })
 });
