0c5b6083268a83c21b27b37c5e79e409fe540ac5
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-05-29 03:29:29 +0800

    add deployment script for zestymarket

---
+++ scripts/deploy-ZestyMarket.js
@@ -0,0 +1,30 @@
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
+async function main() {
+  // rinkeby usdc address 
+  let ERC20Address = "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b";
+
+  // ethereum mainnet usdc address
+  // let ERC20Address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+
+  // matic usdc address
+  // let ERC20Address = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+
+  const ZestyNFT = await hre.ethers.getContractFactory("ZestyNFT");
+  const zestyNFT = await ZestyNFT.deploy(ethers.constants.AddressZero);
+  await zestyNFT.deployed();
+  console.log("ZestyNFT deployed to:", zestyNFT.address);
+
+  const ZestyMarket = await hre.ethers.getContractFactory("ZestyMarket_ERC20_V1");
+  const zestyMarket = await ZestyMarket.deploy(ERC20Address, zestyNFT.address);
+  await zestyMarket.deployed();
+  console.log("ZestyMarket_ERC20_V1 deployed to:", zestyMarket.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
