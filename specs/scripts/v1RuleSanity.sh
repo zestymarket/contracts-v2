e2bad8d13986a67aaccf545292da45f506117046
Shelly Grossman <shelly6.g@gmail.com>
2021-06-25 02:47:28 +0300

    script oops

--- specs/scripts/v1RuleSanity.sh
+++ specs/scripts/v1RuleSanity.sh
@@ -6,5 +6,5 @@ certoraRun.py specs/harness/V1Harness.sol contracts/market/ZestyNFT.sol specs/ha
     --rule $1 \
     --rule_sanity \
     --cache zesty \
-    --msg "Market V1 $1 $2" #\
-#    --staging shelly/preferIteAsExprInHavocAssuming
+    --msg "Market V1 $1 $2" \
+--staging shelly/preferIteAsExprInHavocAssuming
