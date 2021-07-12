9b94c2e1fabcc2cc8336af9e03c1a6710f0a2c43
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-07-13 01:00:58 +0800

    update scripts for matic deployment

--- scripts/deploy-ZestyMarketV1_1.js
+++ scripts/deploy-ZestyMarketV1_1.js
@@ -3,16 +3,19 @@ const { ethers } = require("hardhat");
 
 async function main() {
   // rinkeby usdc address 
-  let ERC20Address = "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b";
+  // let ERC20Address = "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b";
 
   // ethereum mainnet usdc address
   // let ERC20Address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
 
   // matic usdc address
-  // let ERC20Address = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+  let ERC20Address = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
 
   const ZestyNFT = await hre.ethers.getContractFactory("ZestyNFT");
-  const zestyNFT = await ZestyNFT.deploy("0xC22F22d096D578148FFDbe969E89b0fe5Cb9d5aB", ethers.constants.AddressZero);
+  const zestyNFT = await ZestyNFT.deploy(
+    "0xB0270654a0158c8aD52a955f7Fd399474B2107a5", 
+    ethers.constants.AddressZero
+  );
   await zestyNFT.deployed();
   console.log("ZestyNFT deployed to:", zestyNFT.address);
 
