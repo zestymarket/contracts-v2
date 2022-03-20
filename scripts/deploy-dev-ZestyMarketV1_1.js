972ea7cdb8d8fff77af412b118ab24069e1d8299
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2022-03-21 02:38:17 +0800

    more function calls for dev script

--- scripts/deploy-dev-ZestyMarketV1_1.js
+++ scripts/deploy-dev-ZestyMarketV1_1.js
@@ -26,36 +26,107 @@ async function main() {
   await zestyNFT.mint('testUri1');
   await zestyNFT.mint('testUri2');
   await zestyNFT.approve(zestyMarket.address, 0);
-  await zestyToken.approve(zestyMarket.address, 100000000);
-  await zestyToken.transfer(signers[1].address, 100000);
-  await zestyToken.connect(signers[1]).approve(zestyMarket.address, 100000000);
-  await zestyToken.transfer(signers[2].address, 100000);
-  await zestyToken.connect(signers[2]).approve(zestyMarket.address, 100000000);
-  await zestyToken.transfer(signers[3].address, 100000);
-  await zestyToken.connect(signers[3]).approve(zestyMarket.address, 100000000);
+  await zestyToken.approve(zestyMarket.address, 100000000000);
+  await zestyToken.transfer(signers[1].address, 100000000);
+  await zestyToken.connect(signers[1]).approve(zestyMarket.address, 100000000000);
+  await zestyToken.transfer(signers[2].address, 100000000);
+  await zestyToken.connect(signers[2]).approve(zestyMarket.address, 100000000000);
+  await zestyToken.transfer(signers[3].address, 100000000);
+  await zestyToken.connect(signers[3]).approve(zestyMarket.address, 100000000000);
 
   await zestyMarket.connect(signers[1]).buyerCampaignCreate('testUri');
 
-  await zestyMarket.sellerNFTDeposit(0, 2);
+  await zestyMarket.sellerNFTDeposit(0, 1);
 
   await zestyMarket.sellerAuctionCreateBatch(
     0,
-    [timeNow + 100],
+    [timeNow + 300],
     [timeNow + 10000],
-    [timeNow + 101],
+    [timeNow + 301],
     [timeNow + 10001],
     [100]
   );
 
-  await time.increase(200);
+  await time.increase(500);
 
-  await zestyMarket.connect(signers[1]).sellerAuctionBidBatch([1], 1)
+  await zestyMarket.connect(signers[1]).sellerAuctionBidBatch([1], 1);
+
+  await zestyMarket.sellerAuctionRejectBatch([1]);
+
+  await zestyMarket.connect(signers[1]).sellerAuctionBidBatch([1], 1);
+
+  await zestyMarket.sellerAuctionApproveBatch([1]);
 
   time.increase(10050);
 
   await zestyMarket.contractWithdrawBatch([1]);
 
   await zestyMarket.sellerNFTWithdraw(0);
+
+  timeNow = await time.latest();
+  timeNow = timeNow.toNumber();
+
+  await zestyNFT.connect(signers[1]).mint('testUri');
+  await zestyNFT.connect(signers[1]).approve(zestyMarket.address, 3);
+  await zestyMarket.connect(signers[1]).sellerNFTDeposit(3, 2);
+  await zestyMarket.buyerCampaignCreate('testUri');
+
+  await zestyMarket.connect(signers[1]).sellerAuctionCreateBatch(
+    3,
+    [timeNow + 300],
+    [timeNow + 10000],
+    [timeNow + 301],
+    [timeNow + 10001],
+    [100]
+  );
+
+  await time.increase(500);
+
+  await zestyMarket.sellerAuctionBidBatch([2], 2);
+
+  time.increase(10050);
+
+  await zestyMarket.connect(signers[1]).contractWithdrawBatch([2]);
+
+  await zestyNFT.approve(zestyMarket.address, 0);
+  await zestyMarket.sellerNFTDeposit(0, 2);
+
+  timeNow = await time.latest();
+  timeNow = timeNow.toNumber();
+
+  await zestyMarket.sellerAuctionCreateBatch(
+    0,
+    [timeNow + 300],
+    [timeNow + 10000],
+    [timeNow + 301],
+    [timeNow + 10001],
+    [100]
+  );
+
+  await time.increase(600);
+
+  await zestyMarket.connect(signers[1]).sellerAuctionBidBatch([3], 1)
+
+  time.increase(10050);
+
+  await zestyMarket.contractWithdrawBatch([3]);
+
+  timeNow = await time.latest();
+  timeNow = timeNow.toNumber();
+
+  await zestyNFT.connect(signers[2]).mint('testUri');
+  await zestyNFT.connect(signers[2]).approve(zestyMarket.address, 4);
+  await zestyMarket.connect(signers[2]).sellerNFTDeposit(4, 1);
+  await zestyMarket.connect(signers[2]).sellerAuctionCreateBatch(
+    4,
+    [timeNow + 300, timeNow + 300],
+    [timeNow + 10000, timeNow + 10000],
+    [timeNow + 301, timeNow + 301],
+    [timeNow + 10001, timeNow + 10001],
+    [100, 100]
+  );
+  await time.increase(600);
+  await zestyMarket.connect(signers[1]).sellerAuctionBidBatch([4], 1);
 }
 
 main()
