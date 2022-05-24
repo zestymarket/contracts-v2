35b743bf640a1396c2bdf20680de6dba8f4837b5
cryptometa <cryptometa>
2022-05-24 09:35:17 -0400

    Payment splitter added

--- contracts/market/ZestyRevenueShareWrapper.sol
+++ contracts/market/ZestyRevenueShareWrapper.sol
@@ -4,6 +4,7 @@ pragma experimental ABIEncoderV2;
 
 import "../utils/ReentrancyGuard.sol";
 import "../utils/EnumerableSet.sol";
+import "../utils/PaymentSplitter.sol";
 import "../interfaces/IZestyMarket_ERC20_V1_1.sol";
 import "../interfaces/IZestyNFT.sol";
 
@@ -11,15 +12,17 @@ contract ZestyRevenueShareWrapper is ReentrancyGuard {
   using EnumerableSet for EnumerableSet.AddressSet;
 
   IZestyMarket_ERC20_V1_1 public zestyMarketAddress;
+  PaymentSplitter public paymentSplitter;
   mapping(uint256 => address) public owners;
-  mapping(uint256 => EnumerableSet.AddressSet) private _recipients;
 
-  constructor(address zestyMarketAddress_) {
+  constructor(address zestyMarketAddress_, address payable paymentSplitter_) {
     zestyMarketAddress = IZestyMarket_ERC20_V1_1(zestyMarketAddress_);
+    paymentSplitter = PaymentSplitter(paymentSplitter_);
   }
 
-  function sellerNFTDeposit(uint256 _tokenId, uint8 _autoApprove, address[] calldata recipients_) external nonReentrant {
+  function sellerNFTDeposit(uint256 _tokenId, uint8 _autoApprove, address[] calldata recipients_, uint256[] calldata shares_) external nonReentrant {
     require(recipients_.length <= 20, "Too many recipients");
+    require(recipients_.length == shares_.length, "Length mismatch");
     IZestyNFT _zestyNFT = IZestyNFT(zestyMarketAddress.getZestyNFTAddress());
     require(
       _zestyNFT.ownerOf(_tokenId) == msg.sender &&
@@ -28,9 +31,7 @@ contract ZestyRevenueShareWrapper is ReentrancyGuard {
     );
 
     owners[_tokenId] = msg.sender;
-    for(uint256 i = 0; i < recipients_.length; i ++) {
-      _recipients[_tokenId].add(recipients_[i]);
-    }
+    paymentSplitter.addPayees(recipients_, shares_);
 
     _zestyNFT.safeTransferFrom(msg.sender, address(this), _tokenId);
     _zestyNFT.approve(address(zestyMarketAddress), _tokenId);
@@ -42,20 +43,22 @@ contract ZestyRevenueShareWrapper is ReentrancyGuard {
     IZestyNFT _zestyNFT = IZestyNFT(zestyMarketAddress.getZestyNFTAddress());
     _zestyNFT.safeTransferFrom(address(this), msg.sender, _tokenId);
     owners[_tokenId] = address(0);
-    // clear recipients
-    uint256 length = _recipients[_tokenId].length();
-    for(uint256 i = 0; i < length; i ++) {
-      _recipients[_tokenId].remove(_recipients[_tokenId].at(0));
-    }
+    // clear recipients TO-do
+    // uint256 length = _recipients[_tokenId].length();
+    // for(uint256 i = 0; i < length; i ++) {
+    //   _recipients[_tokenId].remove(_recipients[_tokenId].at(0));
+    // }
   }
 
+  // To-do
   function addRecipient(uint256 _tokenId, address newRecipient) external onlyDepositOwner(_tokenId) returns(bool success) {
-    require(_recipients[_tokenId].length() < 20, "Too many recipients");
-    success = _recipients[_tokenId].add(newRecipient);
+    // require(_recipients[_tokenId].length() < 20, "Too many recipients");
+    // success = _recipients[_tokenId].add(newRecipient);
   }
 
+  // To-do
   function removeRecipient(uint256 _tokenId, address oldRecipient) external onlyDepositOwner(_tokenId) returns(bool success) {
-    success = _recipients[_tokenId].remove(oldRecipient);
+    // success = _recipients[_tokenId].remove(oldRecipient);
   }
 
   modifier onlyDepositOwner(uint256 _tokenId) {
