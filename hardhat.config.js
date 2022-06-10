3e61bd7719bc7139105233fecc72f3eef64627ca
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2022-06-11 03:45:30 +0800

    add 0.8.4

--- hardhat.config.js
+++ hardhat.config.js
@@ -31,6 +31,15 @@ module.exports = {
           }
         }
       },
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
     ]
   },
   networks: {
